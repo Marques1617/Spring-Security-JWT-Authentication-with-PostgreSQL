1a89b04aad38e80a49194273ca18c9a8f75ce3d0
JoaoMarques_17 <jpinhom@gmail.com>
2026-01-22 10:05:03 +0000

    Lombok -> Student Class

--- SpringSecurity/src/main/java/com/marques/SpringSecurity/Model/Student.java
+++ SpringSecurity/src/main/java/com/marques/SpringSecurity/Model/Student.java
@@ -1,5 +1,8 @@
 package com.marques.SpringSecurity.Model;
 
+import lombok.Data;
+
+@Data
 public class Student {
     private int id;
     private String name;
@@ -8,30 +11,6 @@ public class Student {
     public Student(int id, String name, String email) {
         this.id = id;
         this.name = name;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
     
